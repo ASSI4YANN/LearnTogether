18eac9bc158cd9cb89c607a63034c5d288ba03ed
ASSI4YANN <nassiyannjunior@gmail.com>
2025-08-11 22:38:45 +0000

    Premier commit

---
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.learntogether
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learntogether.ui.theme.LearnTogetherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LearnTogetherTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LearnTogetherText(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    fun LearnTogetherText(modifier: Modifier = Modifier) {
+        val image = painterResource(R.drawable.bg_compose_background)
+        Column {
+            Row() {
+                Image(
+                    painter = image,
+                    contentDescription = null
+                )
+            }
+
+            Text(
+                text = stringResource(R.string.text_1),
+                fontSize = 24.sp,
+                modifier = Modifier.padding(
+                    16.dp
+                )
+            )
+            Text(
+                text = stringResource(R.string.text_2),
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                textAlign = TextAlign.Justify
+            )
+            Text(
+                text = stringResource(R.string.text_3),
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Justify
+            )
+
+        }
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun LearnCardPreview() {
+        LearnTogetherTheme {
+            LearnTogetherText()
+        }
+    }
+}
